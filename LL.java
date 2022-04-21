ed408a436208c7c21c9acae9717dc7bc7aa06cce
Prakhar Sharma <Prakhar.sharma_cs20@gla.ac.in>
2022-04-22 01:02:40 +0530

    https://github.com/Prakhar788/Data-StructuresAlgorithms/tree/main/LinkedList

--- LL.java
+++ LL.java
@@ -133,10 +133,7 @@ public class LL {
 public static void main(String[] args) {
     LL list=new LL();
     list.insertFirst(3);
-    list.insertFirst(4);
+    list.insertFirst(5);
     list.display();
 }
 }
-
-
-
