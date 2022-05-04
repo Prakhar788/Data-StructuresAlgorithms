3dcbb609ac2007e079efc1f1308592b7fce6d261
Prakhar Sharma <Prakhar.sharma_cs20@gla.ac.in>
2022-05-04 20:38:28 +0530

    https://github.com/Prakhar788/Data-StructuresAlgorithmshttps://github.com/Prakhar788/Data-StructuresAlgorithms

--- Tree/BST/BinarySearchTree.java
+++ Tree/BST/BinarySearchTree.java
@@ -1,8 +1,6 @@
-//Java program to demonstrate delete operation in binary search tree
+
 class BinarySearchTree {
-	/* Class containing left
-	and right child of current node
-	* and key value*/
+	
 	class Node {
 		int key;
 		Node left, right;
@@ -14,45 +12,40 @@ class BinarySearchTree {
 		}
 	}
 
-	// Root of BST
+	
 	Node root;
 
-	// Constructor
+	
 	BinarySearchTree() { root = null; }
 
-	// This method mainly calls deleteRec()
+	
 	void deleteKey(int key) { root = deleteRec(root, key); }
 
-	/* A recursive function to
-	delete an existing key in BST
-	*/
+	
 	Node deleteRec(Node root, int key)
 	{
-		/* Base Case: If the tree is empty */
+		
 		if (root == null)
 			return root;
 
-		/* Otherwise, recur down the tree */
+		
 		if (key < root.key)
 			root.left = deleteRec(root.left, key);
 		else if (key > root.key)
 			root.right = deleteRec(root.right, key);
 
-		// if key is same as root's
-		// key, then This is the
-		// node to be deleted
+		
 		else {
-			// node with only one child or no child
+			
 			if (root.left == null)
 				return root.right;
 			else if (root.right == null)
 				return root.left;
 
-			// node with two children: Get the inorder
-			// successor (smallest in the right subtree)
+			
 			root.key = minValue(root.right);
 
-			// Delete the inorder successor
+			
 			root.right = deleteRec(root.right, root.key);
 		}
 
@@ -70,35 +63,33 @@ class BinarySearchTree {
 		return minv;
 	}
 
-	// This method mainly calls insertRec()
+	
 	void insert(int key) { root = insertRec(root, key); }
 
-	/* A recursive function to
-	insert a new key in BST */
+	
 	Node insertRec(Node root, int key)
 	{
 
-		/* If the tree is empty,
-		return a new node */
+		
 		if (root == null) {
 			root = new Node(key);
 			return root;
 		}
 
-		/* Otherwise, recur down the tree */
+		
 		if (key < root.key)
 			root.left = insertRec(root.left, key);
 		else if (key > root.key)
 			root.right = insertRec(root.right, key);
 
-		/* return the (unchanged) node pointer */
+		
 		return root;
 	}
 
-	// This method mainly calls InorderRec()
+	
 	void inorder() { inorderRec(root); }
 
-	// A utility function to do inorder traversal of BST
+	
 	void inorderRec(Node root)
 	{
 		if (root != null) {
@@ -108,7 +99,7 @@ class BinarySearchTree {
 		}
 	}
 
-	// Driver Code
+	
 	public static void main(String[] args)
 	{
 		BinarySearchTree tree = new BinarySearchTree();
