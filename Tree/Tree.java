65c6f4913d423b7ab3f223bb394d47c0d8cd8565
Prakhar Sharma <Prakhar.sharma_cs20@gla.ac.in>
2022-05-16 12:10:46 +0530

    https://github.com/Prakhar788/Data-StructuresAlgorithms

--- Tree/Tree.java
+++ Tree/Tree.java
@@ -46,7 +46,7 @@ public class Tree {
 	
 	static void preOrder(Node root) {
 		if(root == null) return;
-		System.out.print(root.data+" ");
+		 System.out.print(root.data+" ");
 		preOrder(root.left);
 		preOrder(root.right);
 	}
