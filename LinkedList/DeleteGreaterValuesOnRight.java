9b823993508ddf6f35762bf35fa368b64d74fdae
Prakhar Sharma <Prakhar.sharma_cs20@gla.ac.in>
2022-05-31 06:37:01 +0530

    Node delete(Node head) {         if(head==null)             return head;         Node temp = head;        while(temp!=null && temp.next!=null)        {            if(temp.data< temp.next.data) {                temp.data=temp.next.data;                temp.next=temp.next.next;            }            else                temp=temp.next;         }         return head;     }

--- LinkedList/DeleteGreaterValuesOnRight.java
+++ LinkedList/DeleteGreaterValuesOnRight.java
@@ -29,4 +29,21 @@ class Solution
         return reverse(dummy.next);
     }
 }
+
+
+// Node delete(Node head) {
+//     if(head==null)
+//     return head;
+// Node temp = head;
+// while(temp!=null && temp.next!=null)
+// {
+//    if(temp.data< temp.next.data) {
+//        temp.data=temp.next.data;
+//        temp.next=temp.next.next;
+//    }
+//    else
+//        temp=temp.next;
+// }
+// return head;
+// }
   
