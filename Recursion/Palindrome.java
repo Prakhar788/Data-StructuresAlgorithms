7025af28dd75a8182f1fc576e782f297eabb0a9c
Prakhar Sharma <Prakhar.sharma_cs20@gla.ac.in>
2022-06-20 21:26:23 +0530

    https://github.com/Prakhar788/Data-StructuresAlgorithms

--- Recursion/Palindrome.java
+++ Recursion/Palindrome.java
@@ -5,7 +5,7 @@ public class Palindrome {
     public static int rev(int n) {
         int digits=(int)(Math.log10(n))+1;
         return rev2(n,digits);
-        
+         
     }
 
     private static int rev2(int n, int digits) {
