67a315250e04c0a5ae22eaf0cfda0d2c923a69f5
Prakhar Sharma <Prakhar.sharma_cs20@gla.ac.in>
2022-06-21 20:21:30 +0530

    https://github.com/Prakhar788/Data-StructuresAlgorithms

--- Recursion/Sumofdigits.java
+++ Recursion/Sumofdigits.java
@@ -3,19 +3,19 @@
 class sum {
 
 	
-	static int sumDigits(int no)
+	static int sumDigits(int n)
 	{
-		if(no == 0){
+		if(n == 0){
 		return 0 ;
 		}
 
-		return (no % 10) + sumDigits(no / 10) ;
+		return (n % 10) + sumDigits(n / 10) ;
 	}
 
 	
 	public static void main(String[] args)
 	{
-		System.out.println(sumDigits(687));
+		System.out.println(sumDigits(743));
 	}
 }
 
