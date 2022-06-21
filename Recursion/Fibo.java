d8e09170cde7e6609924c09aa48cea5e9a9dcdde
Prakhar Sharma <Prakhar.sharma_cs20@gla.ac.in>
2022-06-21 12:04:05 +0530

    https://github.com/Prakhar788/Data-StructuresAlgorithms

--- Recursion/Fibo.java
+++ Recursion/Fibo.java
@@ -2,12 +2,11 @@ public class Fibo {
     public static void main(String[] args) {
        System.out.println(fibo(6)); 
     }
-    
-}
-static int fibo(int n)
-{
+    static int fibo(int n)
+ {
     if(n<2){
         return n;
     }
     return fibo(n-1)+fibo(n-2);
+ }
 }
